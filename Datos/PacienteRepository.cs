03dcfbc8621a7aaecdfa1481d4b216f4a7d7e170
Augusto0414 <augustopelis4@gmail.com>
2021-11-18 08:54:08 -0500

    creacion de las clase pasiente service y pasiente repository

---
+++ Datos/PacienteRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+namespace Datos
+{
+   public class PacienteRepository
+    {
+        DbConnection _connection;
+        public PacienteRepository(DbConnection connection)
+        {
+                _connection = connection;
+        }
+    }
+}
---
+++ Logica/PacienteService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Logica
+{
+    public class PacienteService
+    {
+        PacienteRepository pacienteRepository;
+        ConnectionManager connectionManager;
+        public PacienteService(string _conn)
+        {
+            connectionManager = new ConnectionManager(_conn);
+            pacienteRepository = new PacienteRepository(connectionManager.conn);
+            
+        }
+    }
+}
